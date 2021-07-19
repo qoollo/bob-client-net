72e98ce415583442144a41a3398e23f36bfeb030
ikopylov <ikopylov@users.noreply.github.com>
2021-07-19 22:48:45 +0300

    Test app improvement to perform real perf tests

--- BobClient/Qoollo.BobClient.App/Program.cs
+++ BobClient/Qoollo.BobClient.App/Program.cs
@@ -23,83 +23,171 @@ namespace Qoollo.BobClient.App
         class ExecutionConfig
         {
             public RunMode RunMode { get; set; } = RunMode.Get | RunMode.Exists;
-            public int DataLength { get; set; } = 1024;
+            public int? DataLength { get; set; } = null;
             public ulong StartId { get; set; } = 0;
-            public int Count { get; set; } = 1000;
+            public ulong? EndId { get; set; } = null;
+            public uint Count { get; set; } = 1000;
+            public bool RandomMode { get; set; } = false;
+            public bool Verbose { get; set; } = false;
+            public int Timeout { get; set; } = 60;
+            public uint ThreadCount { get; set; } = 1;
+            public uint ExistsPackageSize { get; set; } = 100;
             public List<string> Nodes { get; set; } = new List<string>();
         }
 
-        static void PutTest(IBobApi<ulong> client, ulong startId, int count, byte[] data)
+        static void PutTest(IBobApi<ulong> client, ulong startId, ulong endId, uint count, uint threadCount, bool randomWrite, bool verbose, byte[] data)
         {
-            Stopwatch sw = Stopwatch.StartNew();
-            for (int i = 0; i < count; i++)
-            {
-                try
-                {
-                    client.Put(startId + (ulong)i, data, default(CancellationToken));
-                    if (i % 100 == 0)
-                        Console.WriteLine($"Put {startId + (ulong)i}: Ok");
-                }
-                catch
-                {
-                    Console.WriteLine($"Put {startId + (ulong)i}: Error");
-                }
-            }
+            if (endId < startId || count > endId - startId)
+                endId = startId + count;
 
-            Console.WriteLine($"Put finished in {sw.ElapsedMilliseconds}ms. Rps: {(double)(1000 * count) / sw.ElapsedMilliseconds}");
+            ParallelRandom random = new ParallelRandom((int)threadCount);
+
+            using (var progress = new ProgressTracker(1000, "Put", (int)count).Start())
+            {
+                //for (int i = 0; i < count; i++)
+                Parallel.For(0, (int)count, new ParallelOptions() { MaxDegreeOfParallelism = (int)threadCount },
+                (int i) =>
+                {
+                    ulong currentId = startId + (ulong)i;
+                    if (randomWrite)
+                        currentId = startId + (ulong)random.Next(i, maxValue: (int)(endId - startId));
+
+                    try
+                    {
+                        client.Put(currentId, data, default(CancellationToken));
+                        progress.RegisterSuccess();
+                    }
+                    catch (Exception ex)
+                    {
+                        progress.RegisterError();
+                        if (verbose)
+                        {
+                            Console.WriteLine($"Error ({currentId}): {ex.Message}");
+                            Console.WriteLine(ex.ToString());
+                        }
+                    }
+                });
+
+                progress.Dispose();
+                progress.Print();
+
+                Console.WriteLine($"Put finished in {progress.ElapsedMilliseconds}ms. Rps: {Math.Round((double)(1000 * count) / progress.ElapsedMilliseconds, 2)}");
+                if (progress.CurrentErrorCount > 0)
+                    Console.WriteLine($"Errors: {progress.CurrentErrorCount}");
+                Console.WriteLine();
+            }
         }
 
-        static void GetTest(IBobApi<ulong> client, ulong startId, int count, byte[] expectedData = null)
+        static void GetTest(IBobApi<ulong> client, ulong startId, ulong endId, uint count, uint threadCount, bool randomRead, bool verbose, int? expectedLength = null)
         {
-            Stopwatch sw = Stopwatch.StartNew();
-            for (int i = 0; i < count; i++)
-            {
-                try
-                {
-                    var result = client.Get(startId + (ulong)i, token: default(CancellationToken));
-                    if (expectedData != null && result.Length != expectedData.Length)
-                        Console.WriteLine("Result length mismatch");
-                    if (i % 100 == 0)
-                        Console.WriteLine($"Get {startId + (ulong)i}: Ok");
-                }
-                catch (BobKeyNotFoundException)
-                {
-                    Console.WriteLine($"Get {startId + (ulong)i}: Key not found");
-                }
-                catch
-                {
-                    Console.WriteLine($"Get {startId + (ulong)i}: Error");
-                }
-            }
+            if (endId < startId || count > endId - startId)
+                endId = startId + count;
 
-            Console.WriteLine($"Get finished in {sw.ElapsedMilliseconds}ms. Rps: {(double)(1000 * count) / sw.ElapsedMilliseconds}");
+            ParallelRandom random = new ParallelRandom((int)threadCount);
+
+            using (var progress = new ProgressTracker(1000, "Get", (int)count).Start())
+            {
+                int keyNotFoundErrors = 0;
+                int lengthMismatchErrors = 0;
+                int otherErrors = 0;
+
+                //for (uint i = 0; i < count; i++)
+                Parallel.For(0, (int)count, new ParallelOptions() { MaxDegreeOfParallelism = (int)threadCount },
+                (int i) =>
+                {
+                    ulong currentId = startId + (ulong)i;
+                    if (randomRead)
+                        currentId = startId + (ulong)random.Next(i, maxValue: (int)(endId - startId));
+
+                    try
+                    {
+                        var result = client.Get(currentId, token: default(CancellationToken));
+                        if (expectedLength != null && expectedLength.Value != result.Length)
+                        {
+                            lengthMismatchErrors++;
+                            progress.RegisterError();
+                            if (verbose)
+                                Console.WriteLine($"Error ({currentId}): Length mismatch. Expected length: {expectedLength.Value}, actual: {result.Length}");
+                        }
+                        else
+                        {
+                            progress.RegisterSuccess();
+                        }
+                    }
+                    catch (BobKeyNotFoundException)
+                    {
+                        keyNotFoundErrors++;
+                        progress.RegisterError();
+                        if (verbose)
+                            Console.WriteLine($"Error ({currentId}): Key not found");
+                    }
+                    catch (Exception ex)
+                    {
+                        otherErrors++;
+                        progress.RegisterError();
+                        if (verbose)
+                        {
+                            Console.WriteLine($"Error ({currentId}): {ex.Message}");
+                            Console.WriteLine(ex.ToString());
+                        }
+                    }
+                });
+                progress.Dispose();
+                progress.Print();
+
+                Console.WriteLine($"Get finished in {progress.ElapsedMilliseconds}ms. Rps: {Math.Round((double)(1000 * count) / progress.ElapsedMilliseconds, 2)}");
+                if (progress.CurrentErrorCount > 0)
+                    Console.WriteLine($"KeyNotFound: {keyNotFoundErrors}, LengthMismatch: {lengthMismatchErrors}, OtherErrors: {otherErrors}");
+                Console.WriteLine();
+            }
         }
 
-        static void ExistsTest(IBobApi<ulong> client, ulong startId, int count)
+        static void ExistsTest(IBobApi<ulong> client, ulong startId, ulong endId, uint count, uint threadCount, uint packageSize, bool verbose)
         {
-            const int packageSize = 100;
+            if (endId < startId || count > endId - startId)
+                endId = startId + count;
 
-            Stopwatch sw = Stopwatch.StartNew();
+            int expectedRequestsCount = (int)((count - 1) / packageSize) + 1;
+            int totalExistedCount = 0;
 
-            for (int i = 0; i < count; i += packageSize)
+            using (var progress = new ProgressTracker(1000, "Exists", (int)count, () => $"Result: {Volatile.Read(ref totalExistedCount),8}/{count}").Start())
             {
-                ulong[] ids = new ulong[Math.Min(packageSize, count - i)];
-                for (int j = 0; j < ids.Length; j++)
-                    ids[j] = startId + (ulong)i + (ulong)j;
+                //for (int pckgNum = 0; pckgNum < expectedRequestsCount; pckgNum++)
+                Parallel.For(0, expectedRequestsCount, new ParallelOptions() { MaxDegreeOfParallelism = (int)threadCount },
+                (int pckgNum) =>
+                {
+                    int i = (int)(pckgNum * packageSize);
+                    ulong[] ids = new ulong[Math.Min(packageSize, count - i)];
+                    for (int j = 0; j < ids.Length; j++)
+                        ids[j] = startId + (ulong)i + (ulong)j;
 
-                try
-                {
-                    var result = client.Exists(ids, token: default(CancellationToken));
-                    int existedCount = result.Count(o => o == true);
-                    Console.WriteLine($"Exists {startId + (ulong)i} - {startId + (ulong)i + (ulong)ids.Length}: {existedCount}/{ids.Length}");
-                }
-                catch
-                {
-                    Console.WriteLine($"Exists {startId + (ulong)i} - {startId + (ulong)i + (ulong)ids.Length}: Error");
-                }
+                    try
+                    {
+                        var result = client.Exists(ids, token: default(CancellationToken));
+                        int existedCount = result.Count(o => o == true);
+                        Interlocked.Add(ref totalExistedCount, existedCount);
+                        progress.RegisterEvents(ids.Length, isError: false);
+                    }
+                    catch (Exception ex)
+                    {
+                        progress.RegisterEvents(ids.Length, isError: true);
+                        if (verbose)
+                        {
+                            Console.WriteLine($"Error ({ids[0]} - {ids[ids.Length - 1]}): {ex.Message}");
+                            Console.WriteLine(ex.ToString());
+                        }
+                    }
+                });
+
+                progress.Dispose();
+                progress.Print();
+
+                Console.WriteLine($"Exists finished in {progress.ElapsedMilliseconds}ms. Rps for records: {Math.Round((double)(1000 * count) / progress.ElapsedMilliseconds, 2)}. Rps for packages: {Math.Round((double)(1000 * expectedRequestsCount) / progress.ElapsedMilliseconds, 2)}");
+                Console.WriteLine($"Exists result: {totalExistedCount}/{count}");
+                if (progress.CurrentErrorCount > 0)
+                    Console.WriteLine($"Errors: {progress.CurrentErrorCount}");
+                Console.WriteLine();
             }
-
-            Console.WriteLine($"Exists finished in {sw.ElapsedMilliseconds}ms. Rps: {(double)(1000 * count) / sw.ElapsedMilliseconds}");
         }
 
 
@@ -107,10 +195,16 @@ namespace Qoollo.BobClient.App
         {
             Console.WriteLine("Bob client tests");
             Console.WriteLine("Arguments:");
-            Console.WriteLine("  --mode   | -m  : Work mode combined by comma. Possible values: 'Get,Put,Exists'");
-            Console.WriteLine("  --length | -l  : Set size of the single record");
-            Console.WriteLine("  --start  | -s  : Start Id");
-            Console.WriteLine("  --count  | -c  : Count of ids to process");
+            Console.WriteLine("  --mode   | -m  : Work mode combined by comma. Possible values: 'Get,Put,Exists'. Default: 'Get,Exists'");
+            Console.WriteLine("  --length | -l  : Set size of the single record. Default: 1024");
+            Console.WriteLine("  --start  | -s  : Start Id. Default: 0");
+            Console.WriteLine("  --end    | -e  : End Id (optional)");
+            Console.WriteLine("  --count  | -c  : Count of ids to process. Default: 1000");
+            Console.WriteLine("  --threads      : Number of threads. Default: 1");
+            Console.WriteLine("  --timeout      : Timeout in seconds. Default: 60");
+            Console.WriteLine("  --random       : Random read/write mode. Default: false");
+            Console.WriteLine("  --verbose      : Enable verbose output for errors. Default: false");
+            Console.WriteLine("  --packageSize  : Exists package size. Default: 100");
             Console.WriteLine("  --nodes        : Comma separated node addresses. Example: '127.0.0.1:20000, 127.0.0.2:20000'");
             Console.WriteLine("  --help         : Print help");
             Console.WriteLine();
@@ -126,7 +220,7 @@ namespace Qoollo.BobClient.App
                 {
                     case "--mode":
                     case "-m":
-                        result.RunMode = (RunMode)Enum.Parse(typeof(RunMode), args[i + 1]);
+                        result.RunMode = (RunMode)Enum.Parse(typeof(RunMode), args[i + 1], ignoreCase: true);
                         i++;
                         break;
                     case "--length":
@@ -139,10 +233,49 @@ namespace Qoollo.BobClient.App
                         result.StartId = ulong.Parse(args[i + 1]);
                         i++;
                         break;
+                    case "--end":
+                    case "-e":
+                        result.EndId = ulong.Parse(args[i + 1]);
+                        i++;
+                        break;
                     case "--count":
                     case "-c":
-                        result.Count = int.Parse(args[i + 1]);
+                        result.Count = uint.Parse(args[i + 1]);
                         i++;
+                        break;
+                    case "--timeout":
+                        result.Timeout = int.Parse(args[i + 1]);
+                        i++;
+                        break;
+                    case "--threads":
+                        result.ThreadCount = uint.Parse(args[i + 1]);
+                        i++;
+                        break;
+                    case "--packagesize":
+                        result.ExistsPackageSize = uint.Parse(args[i + 1]);
+                        i++;
+                        break;
+                    case "--random":
+                        if (i + 1 < args.Length && bool.TryParse(args[i + 1], out bool randomMode))
+                        {
+                            result.RandomMode = randomMode;
+                            i++;
+                        }
+                        else
+                        {
+                            result.RandomMode = true;
+                        }
+                        break;
+                    case "--verbose":
+                        if (i + 1 < args.Length && bool.TryParse(args[i + 1], out bool verboseMode))
+                        {
+                            result.Verbose = verboseMode;
+                            i++;
+                        }
+                        else
+                        {
+                            result.Verbose = true;
+                        }
                         break;
                     case "--nodes":
                         result.Nodes = args[i + 1].Split(new char[] { ',', ';' }, StringSplitOptions.RemoveEmptyEntries).Select(o => o.Trim()).ToList();
@@ -167,7 +300,13 @@ namespace Qoollo.BobClient.App
                 RunMode = RunMode.Get | RunMode.Put | RunMode.Exists,
                 DataLength = 1024,
                 StartId = 62000,
-                Count = 1000,
+                EndId = null,
+                Count = 20000,
+                ExistsPackageSize = 100,
+                RandomMode = false,
+                Verbose = true,
+                Timeout = 60,
+                ThreadCount = 4,
                 Nodes = new List<string>() { "10.5.5.127:20000", "10.5.5.128:20000" }
             };
 
@@ -181,24 +320,29 @@ namespace Qoollo.BobClient.App
                 return;
             }
 
-            byte[] sampleData = new byte[config.DataLength];
-            for (int i = 0; i < sampleData.Length; i++)
-                sampleData[i] = (byte)(i & byte.MaxValue);
+            byte[] sampleData = null;
+
+            if ((config.RunMode & RunMode.Put) != 0)
+            {
+                sampleData = new byte[config.DataLength ?? 1024];
+                for (int i = 0; i < sampleData.Length; i++)
+                    sampleData[i] = (byte)(i & byte.MaxValue);
+            }
 
 
             using (var client = new BobClusterBuilder<ulong>(config.Nodes)
-                .WithOperationTimeout(TimeSpan.FromSeconds(1000))
+                .WithOperationTimeout(TimeSpan.FromSeconds(config.Timeout))
                 .WithSequentialNodeSelectionPolicy()
                 .Build())
             {
-                client.Open(TimeSpan.FromSeconds(5), BobClusterOpenCloseMode.SkipErrors);
+                client.Open(TimeSpan.FromSeconds(config.Timeout), BobClusterOpenCloseMode.SkipErrors);
 
                 if ((config.RunMode & RunMode.Put) != 0)
-                    PutTest(client, config.StartId, config.Count, sampleData);
+                    PutTest(client, config.StartId, config.EndId ?? (config.StartId + config.Count), config.Count, config.ThreadCount, config.RandomMode, config.Verbose, sampleData);
                 if ((config.RunMode & RunMode.Get) != 0)
-                    GetTest(client, config.StartId, config.Count, expectedData: sampleData);
+                    GetTest(client, config.StartId, config.EndId ?? (config.StartId + config.Count), config.Count, config.ThreadCount, config.RandomMode, config.Verbose, config.DataLength);
                 if ((config.RunMode & RunMode.Exists) != 0)
-                    ExistsTest(client, config.StartId, config.Count);
+                    ExistsTest(client, config.StartId, config.EndId ?? (config.StartId + config.Count), config.Count, config.ThreadCount, config.ExistsPackageSize, config.Verbose);
 
                 client.Close();
             }
---
+++ BobClient/Qoollo.BobClient.App/ProgressTracker.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Qoollo.BobClient.App
+{
+    public class ProgressTracker : IDisposable
+    {
+        private readonly System.Timers.Timer _timer;
+        private volatile bool _isDisposed;
+
+        private volatile int _currentCount;
+        private volatile int _currentErrorCount;
+        private readonly int _totalCount;
+
+        private readonly string _operationDescription;
+
+        private readonly Stopwatch _stopwatch;
+
+        private readonly Func<string> _customMessageBuilder;
+
+        public ProgressTracker(int intervalMs, string operationDescription, int totalCount, Func<string> customMessageBuilder = null)
+        {
+            if (intervalMs <= 0)
+                throw new ArgumentOutOfRangeException(nameof(intervalMs));
+            if (totalCount <= 0)
+                throw new ArgumentOutOfRangeException(nameof(totalCount));
+            if (operationDescription == null)
+                throw new ArgumentNullException(nameof(operationDescription));
+
+            _timer = new System.Timers.Timer(intervalMs);
+            _timer.AutoReset = true;
+            _timer.Enabled = false;
+            _timer.Elapsed += TimerElapsed;
+
+            _totalCount = totalCount;
+            _currentCount = 0;
+            _currentErrorCount = 0;
+
+            _operationDescription = operationDescription;
+
+            _stopwatch = new Stopwatch();
+
+            _customMessageBuilder = customMessageBuilder;
+
+            _isDisposed = false;
+        }
+
+        public int TotalCount { get { return _totalCount; } }
+        public int CurrentCount { get { return _currentCount; } }
+        public int CurrentErrorCount { get { return _currentErrorCount; } }
+
+        public long ElapsedMilliseconds { get { return _stopwatch.ElapsedMilliseconds; } }
+
+        public ProgressTracker Start()
+        {
+            if (!_isDisposed)
+            {
+                _timer.Enabled = true;
+                _stopwatch.Start();
+            }
+
+            return this;
+        }
+
+        public void RegisterEvent(bool isError)
+        {
+            Interlocked.Increment(ref _currentCount);
+            if (isError)
+                Interlocked.Increment(ref _currentErrorCount);
+        }
+        public void RegisterSuccess()
+        {
+            RegisterEvent(false);
+        }
+        public void RegisterError()
+        {
+            RegisterEvent(true);
+        }
+
+        public void RegisterEvents(int count, bool isError)
+        {
+            Interlocked.Add(ref _currentCount, count);
+            if (isError)
+                Interlocked.Add(ref _currentErrorCount, count);
+        }
+
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            Print();
+        }
+
+        public void Print()
+        {
+            int currentErrorCount = _currentErrorCount;
+            long currentCount = _currentCount;
+
+            if (_customMessageBuilder != null)
+                Console.WriteLine($"{_operationDescription}: {currentCount,8}/{TotalCount},   {_customMessageBuilder()},   Errors: {currentErrorCount,5},   RPS: {(currentCount * 1000) / _stopwatch.ElapsedMilliseconds,4}");
+            else
+                Console.WriteLine($"{_operationDescription}: {currentCount,8}/{TotalCount},   Errors: {currentErrorCount,5},   RPS: {(currentCount * 1000) / _stopwatch.ElapsedMilliseconds,4}");
+        }
+
+        public void Dispose()
+        {
+            _isDisposed = true;
+            _stopwatch.Stop();
+            _timer.Stop();
+            _timer.Dispose();
+        }
+    }
+}
