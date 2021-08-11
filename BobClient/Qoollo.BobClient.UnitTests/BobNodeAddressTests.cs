f510cdd0855d6e4159fdc36a5c07cd84099e1e6c
ikopylov <ikopylov@users.noreply.github.com>
2021-08-11 22:45:50 +0300

    BobNodeAddress tests added

---
+++ BobClient/Qoollo.BobClient.UnitTests/BobNodeAddressTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Qoollo.BobClient.UnitTests
+{
+    public class BobNodeAddressTests
+    {
+        [Theory]
+        [InlineData("host:123", "host", 123)]
+        [InlineData("host", "host", BobNodeAddress.DefaultPort)]
+        [InlineData("local_host:500", "local_host", 500)]
+        [InlineData("local_host123:500", "local_host123", 500)]
+        public void ParseTest(string addr, string host, int port)
+        {
+            BobNodeAddress parsed = BobNodeAddress.Parse(addr);
+            Assert.Equal(host, parsed.Host);
+            Assert.Equal(port, parsed.Port);
+        }
+
+        [Theory]
+        [InlineData("host:")]
+        [InlineData("host:-15")]
+        [InlineData(":22")]
+        [InlineData(":::::")]
+        public void ParseFormatExceptionTest(string addr)
+        {
+            Assert.Throws<FormatException>(() =>
+            {
+                BobNodeAddress.Parse(addr);
+            });
+        }
+    }
+}
